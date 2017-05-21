9ee5dc0db09267c62a4f01793cb607c407a4d5c5
markrogan <marogan@tcd.ie>
2017-05-21 13:29:59 +0100

    prob 0001 finished

--- p0001.py
+++ p0001.py
@@ -1,1 +1,12 @@
-print "proj euler"
+def main():
+    sum=0
+    for i in range(1000):
+        if(i%3==0 or i%5==0):
+            sum+=i
+    print sum
+
+
+if __name__ == "__main__":
+    main()
+else:
+    print("not run as main")
