a87fe19c4b11f5d2da7e6c0eb23e61a1c618c444
markrogan <marogan@tcd.ie>
2017-05-21 16:02:21 +0100

    prob 0003 started

---
+++ p0003.py
@@ -0,0 +1,16 @@
+def main():
+    a=1
+    b=2
+    fibNum=0
+    sum=0
+    while(a < 4000000):
+        if(a%2 == 0):
+            sum+=a
+        a, b = b, a+b
+    print sum
+
+
+if __name__ == "__main__":
+    main()
+else:
+    print("not run as main")
